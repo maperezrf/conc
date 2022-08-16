c2fa077901645a829fec17f485bfb881f482fa6f
Pablo Alejandro Parra <pa.parra12@uniandes.edu.co>
2022-08-15 22:32:49 -0500

    dics.py added

--- conc.py
+++ conc.py
@@ -1,6 +1,14 @@
 import pandas as pd 
+from dics import gcons, f12_vars, f3_vars, f11_vars, nc_vars, siebel_vars
 
 class CONC:
 
+    dfs = []
     def  __init__(self) -> None:
+        pass
+
+    def load_files(self):
+        pass
+
+    def transform_files(self):
         pass
---
+++ dics.py
@@ -0,0 +1,24 @@
+gcons = {
+
+}
+
+f12_vars = {
+'file_name':'input/220816_f12.csv'
+}
+
+f3_vars = {
+
+}
+
+nc_vars = {
+
+}
+
+f11_vars = {
+
+}
+
+siebel_vars = {
+
+}
+
