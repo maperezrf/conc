4b7c4fa4b3b76e667483b4b032addbf8ffb2bd0a
maperezrf <95372481+maperezrf@users.noreply.github.com>
2022-08-17 10:57:11 -0500

    Ea 431 desarrollar metodo de py (#1)
    * transform_files method structure
    
    * minors transform method
    
    * cambios menores
    
    * bug load files fixed
    
    * dic variables updated
    
    * EA-431 done
    
    * cambios menores
    
    * Delete prueba.ipynb
    
    Co-authored-by: Pablo Alejandro Parra <pa.parra12@uniandes.edu.co>
    Co-authored-by: Miguel Angel Perez Roa <maperezr@falabella.com.co>

--- conc.py
+++ conc.py
@@ -1,14 +1,91 @@
 import pandas as pd 
-from dics import gcons, f12_vars, f3_vars, f11_vars, nc_vars, siebel_vars
+from config import path
+from dics import gcons, f12_vars, f3_vars, f11_vars, nc_vars, siebel_vars, ro_vars, mc_vars, en_vars, q_vars
+from os import listdir
+from unidecode import unidecode
 
 class CONC:
 
     dfs = []
+
     def  __init__(self) -> None:
-        pass
+        dcorte = '220816' #TODO input('Ingrese corte: ')
+        self.path = path + '/'+ dcorte 
+        files = listdir(self.path)
+        self.file_name_list = ['f3', 'f11', 'f12', 'nc', 'ro', 'mc', 'en', 'quiebres', 'siebel']
+        self.file_name_dirs = get_dirs(files, self.file_name_list)
 
     def load_files(self):
-        pass
+        self.dfs = []
+        for dir in self.file_name_dirs: 
+            if dir != '':
+                self.dfs.append(pd.read_csv(f'{self.path}/{dir}', sep=';', dtype='object'))
+            else:
+                self.dfs.append(pd.DataFrame())
 
     def transform_files(self):
-        pass
+        # PAP:  f3, f12, ro, en, siebel
+        # MAP: f11, nc, mc, quiebres 
+        col_key = [f3_vars['key'], f11_vars['key'], f12_vars['key'], nc_vars['key'], ro_vars['key'], mc_vars['key'], en_vars['key'], q_vars['key'], siebel_vars['key']]
+        cols_dup = [f3_vars['dkeys'], f11_vars['dkeys'], f12_vars['dkeys'], nc_vars['dkeys'], ro_vars['dkeys'], mc_vars['dkeys'], en_vars['dkeys'], q_vars['dkeys'], siebel_vars['dkeys']]
+        cols_num = [f3_vars['cnum'], f11_vars['cnum'], f12_vars['cnum'], nc_vars['cnum'], ro_vars['cnum'], mc_vars['cnum'], en_vars['cnum'], q_vars['cnum'], siebel_vars['cnum']]
+        cols_string = [f3_vars['cstring'], f11_vars['cstring'], f12_vars['cstring'], nc_vars['cstring'], ro_vars['cstring'], mc_vars['cstring'], en_vars['cstring'], q_vars['cstring'], siebel_vars['cstring']]
+        cols_to_keep = [f3_vars['ckeep'], f11_vars['ckeep'], f12_vars['ckeep'], nc_vars['ckeep'], ro_vars['ckeep'], mc_vars['ckeep'], en_vars['ckeep'], q_vars['ckeep'], siebel_vars['ckeep']]
+
+        for i, df in enumerate(self.dfs):
+            if df.empty == False: 
+                self.dfs[i] = set_prefijo_df(self.dfs[i], self.file_name_list[i])
+                self.dfs[i] = keep_cols(self.dfs[i], cols_to_keep[i])
+                self.dfs[i] = clean_num(self.dfs[i], cols_num[i])
+                self.dfs[i] = clean_string(self.dfs[i], cols_string[i])
+                self.dfs[i] = delete_null_values(self.dfs[i], col_key[i])
+                self.dfs[i] = delete_duplicates(self.dfs[i], cols_dup[i])             
+            
+    def get_dfs(self):
+        return self.dfs 
+
+def set_prefijo_df(df, prefijo): # TODO reescribir en list coprehension
+    col = {}
+    for i in df.columns:
+        col[i] = f'{prefijo}_{i.lower()}'
+    return df.rename(columns=col)
+
+def delete_null_values(df, col): # PAP [x]
+    return df.loc[~df[col].isna()].reset_index(drop=True)
+
+def delete_duplicates(df, col_list): # MAP
+    return df.drop_duplicates(col_list)
+
+def clean_num(df, col_list): # PAP
+    res = df.copy()
+    for col in col_list:
+        res.loc[:, col] = res[col].str.replace(r'([^0-9,])', '', regex=True)
+        res.loc[:, col] = pd.to_numeric(res.loc[:, col] )
+    return res
+
+def clean_string(df, col_list): # MAP
+    res = df.copy()
+    for col in col_list:
+        res[col] = res[col].fillna('nan')
+        res[col] = res[col].apply(unidecode)
+        res[col] = res[col].str.replace(r'([^a-zA-Z0-9-+(). ])', '', regex=True)
+        res[col] = res[col].str.strip()
+        res[col] = res[col].str.upper()
+    return res
+
+def keep_cols(df, cols_to_keep):
+    return df.loc[:, cols_to_keep].reset_index(drop=True)
+
+def get_dirs(files, file_name_list): # PAP [x]
+    lista = []
+    for name in file_name_list:
+        flag = False 
+        for file in files:
+            if file.__contains__(name): 
+                lista.append(file)
+                flag = True
+        if flag: 
+            continue 
+        else: 
+            lista.append('')
+    return lista 
--- dics.py
+++ dics.py
@@ -1,24 +1,82 @@
 gcons = {
-
-}
-
-f12_vars = {
-'file_name':'input/220816_f12.csv'
-}
-
-f3_vars = {
-
-}
-
-nc_vars = {
-
+    'input_path':'input/'
 }
 
 f11_vars = {
+    'key': 'f11_nro_f11',
+    'dkeys' : ['f11_nro_f11', 'f11_upc'],
+    'cnum' : ['f11_total_costo', 'f11_cant_prod'],
+    'cstring' : [ 'f11_propietario', 'f11_nombre_local', 'f11_estado', 'f11_servicio'],
+    'ckeep' : ['f11_nro_f11', 'f11_fecha_creacion' ,'f11_upc', 'f11_propietario', 'f11_cod_local', 
+    'f11_nombre_local', 'f11_grupo', 'f11_estado', 'f11_servicio','f11_cant_prod', 'f11_total_costo' ]
+}
 
+nc_vars = {
+    'key':'nc_cautoriza_nc',
+    'dkeys':['nc_cautoriza_nc', 'nc_sku'],
+    'cnum':['nc_qcantidad', 'nc_monto_sku_nc'],
+    'cstring':['nc_estado_nc', 'nc_local_v_nc'],
+    'ckeep':['nc_cautoriza_nc', 'nc_sku', 'nc_qcantidad', 'nc_monto_sku_nc', 'nc_estado_nc', 'nc_local_v_nc' ]
+}
+
+f12_vars = {
+    'key':'f12_nfolio',
+    'dkeys':['f12_nfolio', 'f12_prd_upc'],
+    'cnum':['f12_qproducto','f12_mprecio'],
+    'cstring':['f12_loc_name','f12_desc_estado','f12_desc_subestado','f12_desc_mt','f12_ind_nc'],
+    'ckeep':['f12_nfolio','f12_loc_id','f12_loc_name','f12_dcreacion','f12_dreparto','f12_dpactada','f12_desc_estado',
+    'f12_desc_subestado','f12_desc_mt','f12_ind_nc','f12_ctipo','f12_cvendedor','f12_bretira_dsp','f12_partition_date',
+    'f12_origin','f12_prd_upc','f12_qproducto','f12_mprecio', 'f12_so']
+}
+
+f3_vars = {
+    'key': 'f3_nro_devolucion',
+    'dkeys':['f3_nro_devolucion', 'f3_upc'],
+    'cnum':['f3_cantidad',  'f3_cant*costoprmd'],
+    'cstring':['f3_nro_devolucion', 'f3_tipo_producto', 'f3_upc', 'f3_sku', 'f3_descripcion', 'f3_local', 'f3_descripcion5', 'f3_estado', 'f3_descripcion6','f3_folio_f12'],
+    'ckeep':['f3_nro_devolucion', 'f3_fecha_reserva', 'f3_fecha_envio', 'f3_fecha_anulacion', 
+    'f3_fecha_confirmacion','f3_tipo_producto', 'f3_upc', 'f3_sku', 'f3_descripcion', 'f3_local', 
+    'f3_descripcion5', 'f3_estado', 'f3_descripcion6', 'f3_cantidad',  'f3_cant*costoprmd', 'f3_folio_f12']
 }
 
 siebel_vars = {
+    'key': 'ss_ss',
+    'dkeys':['ss_suborden'],
+    'cnum':[],
+    'cstring':[],
+    'ckeep':['ss_ss','ss_n1','ss_n2','ss_n3','ss_tipo','ss_estado','ss_subestado','ss_fecha_creacion','ss_fecha_solucion','ss_fecha_cierre',
+    'ss_tienda_origen','ss_orden_compra','ss_suborden','ss_num_f12','ss_area_problema','ss_sucursal_creador','ss_descripcion','ss_solucion']
+}
 
+mc_vars = {
+    'key': 'mc_entrada',
+    'dkeys': ['mc_entrada'],
+    'cstring' : ['mc_tip0_trabajo', 'mc_entrada'],
+    'cnum' : [],
+    'ckeep' : ['mc_tip0_trabajo', 'mc_entrada']
+}
+
+q_vars = { 
+    'key' : 'quiebres_f12',
+    'dkeys' : ['quiebres_f12', 'quiebres_sku', 'quiebres_cantidad_cancelada'],
+    'cnum' : ['quiebres_cost_unit_prod', 'quiebres_cantidad_cancelada'],
+    'cstring': ['quiebres_tipo_quiebre'],
+    'ckeep' : ['quiebres_f12', 'quiebres_sku', 'quiebres_cantidad_cancelada', 'quiebres_cost_unit_prod', 'quiebres_tipo_quiebre']
+}
+
+ro_vars = {
+    'key':'ro_ro',
+    'dkeys':['ro_do_inicial',  'ro_upc'],
+    'cnum':['ro_cantidad_producto'],
+    'cstring':['ro_estado_ro'],
+    'ckeep':[ 'ro_do_inicial',  'ro_upc', 'ro_fecha_creacion_ro',  'ro_ro', 'ro_estado_ro', 'ro_cantidad_producto']
+}
+
+en_vars = {    
+    'key':'en_centrada',
+    'dkeys':['en_centrada'],
+    'cnum':[ 'en_qcantida'],
+    'cstring':['en_darticul' ],
+    'ckeep':[ 'en_centrada','en_fcreareg', 'en_creferen', 'en_darticul',  'en_qcantida']
 }
 
